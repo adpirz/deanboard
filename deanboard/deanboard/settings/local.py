eb29130ff6e27d9bdbb9dca5b37b4472cfac7cf2
Adnan Pirzada <ad.pirz@gmail.com>
2014-10-18 17:19:49 -0500

    added settings

---
+++ deanboard/deanboard/settings/local.py
@@ -0,0 +1,18 @@
+from .base import *
+
+
+DEBUG = True
+
+TEMPLATE_DEBUG = DEBUG
+
+ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'deanboard',
+    }
+}
